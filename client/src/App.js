012179e850d8f862468efa734dcd2de000800c11
Tarun R G <tarungovindharaj2003@gmail.com>
2023-09-23 19:13:09 +0530

    init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h2>Start developing here</h2>
+    </div>
+  );
+}
+export default App;
